92d03dc79bc60624da258dabcba495514988b731
shobin7x <shobin7job@gmail.com>
2024-08-16 23:22:23 +0530

    contact page started

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,10 @@
 import Navbar from "../Navbar"
+import { MainPart } from "./MainPart"
 export const Contact = () => {
     return (
         <div>
             <Navbar />
-            Contact
+            <MainPart />
         </div>
     )
 }
---
+++ src/components/contact/MainPart.jsx
@@ -0,0 +1,10 @@
+export const MainPart = () => {
+    return (
+        <div className="bg-black p-10 relative z-20">
+            <h1 className="mt-20 ml-8 text-5xl font-bold text-sky-500">Please contact us</h1>
+            <div>
+            <input type="text" className="ml-8 w-52 mt-20 h-10 bg-zinc-300" placeholder="Name"></input>
+            </div>
+        </div>
+    )
+}
