3a1d663a34e73c8dbbb4ce4abc85b4caab15d5af
Rohanj48 <rohanjosepharun@gmail.com>
2024-08-16 23:38:20 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import logo from './../assets/logo.png'
 
 const Navbar = () => {
 
-
+    //lg:mx-72 sm:mx-8
     return (
 
-        <nav className="flex justify-between bg-black text-white items-center p-2 overflow-x-hidden fixed z-50 w-full top-0 h-[182px]">
+        <nav className="flex justify-between bg-black text-white items-center p-2 overflow-x-hidden fixed z-50 w-full top-0 h-[130px]">
 
             <div className='flex shrink-0 items-center max-w-screen-xl '>
-                <a><img src={logo} alt='looogo' width={142} height={134} className="" /></a>
+                <a><img src={logo} alt='looogo' width={110} height={100} className="" /></a>
                 <div className='text-2xl mx-4 font-semibold '>
                     <p>Association for </p>
                     <p>Computing Machinery </p>
                 </div>
             </div>
 
-            <ul className="list-none p-0 m-0 flex gap-4 text-3xl lg:mx-72 sm:mx-8 space-x-2 font-semibold">
+            <ul className="list-none p-0 m-0 flex gap-4 text-3xl mr-5 space-x-2 font-semibold">
                 <li className=' hover:text-cyan-600'>
                     <a href='/home'>HOME</a>
                 </li>
--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -2,7 +2,7 @@ import groupimg from "./../../assets/group-photo.jpeg"
 export const AboutHero = () => {
     return (
         <div style={{ backgroundImage: `url(${groupimg}) ` }}
-            className="bg-cover h-[60vh] overflow-x-hidden items-center flex flex-col justify-center sticky top-[182px] z-10"
+            className="bg-cover h-[60vh] overflow-x-hidden items-center flex flex-col justify-center sticky top-[130px] z-10"
         >
             <h1 className='text-6xl text-white text-center font-semibold mx-auto text-nowrap mb-24'>ABOUT US</h1>
         </div>
