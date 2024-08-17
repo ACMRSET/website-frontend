9794d7813a1bee4c6508efbf7535d3d1f14fb893
shobin7x <shobin7job@gmail.com>
2024-08-17 12:30:01 +0530

    final commit maybe

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,10 +3,10 @@ import logo from "./../../assets/unnamed.png"
 export const Hero = () => {
     return (
         <div style={{ backgroundImage: `url(${heroimg}) ` }}
-            className="bg-cover h-[90%] overflow-x-hidden sticky top-[130px] z-10 overflow-hidden"
+            className="bg-cover h-[70vh] overflow-x-hidden sticky top-[130px] z-10 overflow-hidden"
         >
-            <img src={logo} alt="logoimg" width={550} height={300} className="mx-auto py-8" />
-            <p className="lg:text-7xl md:text-4xl sm:text-3xl text-white text-center font-semibold tracking-wide">
+            <img src={logo} alt="logoimg" width={440} height={240} className="mx-auto py-8" />
+            <p className="lg:text-7xl md:text-4xl sm:text-3xl text-white text-center font-bold tracking-wide">
                 Advanced Computing as
                 <br /> a Science & profession
             </p>
