8ec26ec36cd1df0fcacda18294ba52253a366c63
Rohanj48 <rohanjosepharun@gmail.com>
2024-08-19 22:11:05 +0530

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,92 @@
 
+import { useState } from 'react'
 import logo from './../assets/logo.png'
-
+import { Menu, X } from "lucide-react"
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const toggleNavbar = () => {
+
+        setOpen(!open);
+
+    }
 
     //lg:mx-72 sm:mx-8
     return (
+        <>
+            <nav className="flex justify-between bg-black text-white items-center p-2 overflow-x-hidden fixed z-50 w-full top-0 h-[130px]">
 
-        <nav className="flex justify-between bg-black text-white items-center p-2 overflow-x-hidden fixed z-50 w-full top-0 h-[130px]">
-
-            <div className='flex shrink-0 items-center max-w-screen-xl '>
-                <a href="https://www.acm.org/" target="_blank"><img src={logo} alt='looogo' width={110} height={100} className="" /></a>
-                <div className='text-2xl mx-4 font-semibold '>
-                    <p>Association for </p>
-                    <p>Computing Machinery </p>
+                <div className='flex shrink-0 items-center max-w-screen-xl '>
+                    <a href="https://www.acm.org/" target="_blank"><img src={logo} alt='looogo' width={55} height={50} className="md:h-[100] md:w-[110]" /></a>
+                    <div className='text-xl md:text-2xl mx-4 font-semibold '>
+                        <p>Association for </p>
+                        <p>Computing Machinery </p>
+                    </div>
                 </div>
-            </div>
 
-            <ul className="list-none p-0 m-0 flex gap-4 text-xl mr-5 space-x-2 font-semibold flex-wrap ">
-                <li className=' hover:text-cyan-600'>
-                    <a href='/home'>HOME</a>
-                </li>
-                <li className=' hover:text-cyan-600'>
-                    <a href='/about'>ABOUT US</a>
-                </li>
+                <div className='hidden md:flex'>
 
-                <li className=' hover:text-cyan-600'>
-                    <a href='/events'>EVENTS</a>
-                </li >
+                    <ul className="list-none p-0 m-0 flex gap-4 text-xl mr-5 space-x-2 font-semibold flex-wrap ">
+                        <li className=' hover:text-cyan-600'>
+                            <a href='/home'>HOME</a>
+                        </li>
+                        <li className=' hover:text-cyan-600'>
+                            <a href='/about'>ABOUT US</a>
+                        </li>
 
-                <li className=' hover:text-cyan-600'>
-                    <a href='/team'>TEAM</a>
-                </li>
-                <li className=' hover:text-cyan-600'>
-                    <a href='/contact'>CONTACT</a>
-                </li>
+                        <li className=' hover:text-cyan-600'>
+                            <a href='/events'>EVENTS</a>
+                        </li >
 
-            </ul>
+                        <li className=' hover:text-cyan-600'>
+                            <a href='/team'>TEAM</a>
+                        </li>
+                        <li className=' hover:text-cyan-600'>
+                            <a href='/contact'>CONTACT</a>
+                        </li>
 
-        </nav>
+                    </ul>
+
+
+                </div>
+                <div className='flex md:hidden'>
+                    <button onClick={toggleNavbar}>{open ? <X /> : <Menu />}</button>
+                </div>
+
+
+
+
+
+            </nav>
+            {
+                open && (<div className='md:hidden z-40 items-center mb-3'>
+
+                    <ul className="list-none p-0 m-0 grid-cols-1 justify-center gap-4 text-xl mr-5 space-x-2 font-semibold flex-wrap text-white">
+                        <li className=' hover:text-cyan-600 text-center'>
+                            <a href='/home'>HOME</a>
+                        </li>
+                        <li className=' hover:text-cyan-600 text-center'>
+                            <a href='/about'>ABOUT US</a>
+                        </li>
+
+                        <li className=' hover:text-cyan-600 text-center' >
+                            <a href='/events'>EVENTS</a>
+                        </li >
+
+                        <li className=' hover:text-cyan-600 text-center'>
+                            <a href='/team'>TEAM</a>
+                        </li>
+                        <li className=' hover:text-cyan-600 text-center'>
+                            <a href='/contact'>CONTACT</a>
+                        </li>
+
+                    </ul>
+
+
+                </div>)
+            }
+        </>
 
     )
 }
